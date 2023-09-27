945ca6b4c21a68932fe92ec117c1ca3e4661fc88
branham514 <branham.gatheru@student.moringaschool.com>
2023-09-27 16:52:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+
+//Generate a random integer use Math.random()
+//number between 0 - 0.999
+//To check if the number is an integer use Number.isINteger()
+
+let random = Number.isInteger(Math.random() * 10) + 1;
+let num3 = 9;
+let num4 = 5;
+
+let mod = num3%num4;
+
+// To find the maximum number we use Math.max()
+
+let max = Math.max(12,6,7,4,15,17,19,20)
+max;
